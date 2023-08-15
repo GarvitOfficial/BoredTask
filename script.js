1243a00657f0df1310ed0a0316264976aaf48058
Garvit Bhardwaj <68268393+GarvitOfficial@users.noreply.github.com>
2023-08-15 20:51:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fetchActivityBtn = document.getElementById("fetchActivityBtn");
+  const btnText = document.getElementById("btnText");
+  const loadingSpinner = document.getElementById("loadingSpinner");
   const resultDiv = document.getElementById("result");
   const activityElement = document.getElementById("activity");
 
   fetchActivityBtn.addEventListener("click", async () => {
-    const response = await fetch("https://www.boredapi.com/api/activity");
+    btnText.textContent = "Fetching...";
+    loadingSpinner.classList.remove("hidden");
+    resultDiv.classList.add("hidden");
+
+    const response = await fetch("https://www.boredapi.com/api/activity?participants=1");
     const data = await response.json();
+
+    btnText.textContent = "Fetch Activity";
+    loadingSpinner.classList.add("hidden");
 
     if (response.ok) {
       activityElement.textContent = data.activity;
